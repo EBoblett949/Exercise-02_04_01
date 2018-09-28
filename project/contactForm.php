4e18e524aa3b4b8c89feecdd08cf522399994587
Unknown <EBoblett949@west-mec.org>
2018-09-28 15:23:14 -0700

    first project is done
    first project is done

---
+++ project/contactForm.php
@@ -0,0 +1,25 @@
+    <!doctype html>
+<html>
+<head>
+    <!-- 
+        Exercise 02_04_01
+        Author: Eli Boblett
+        Date: 09.28.18 
+        contactForm.html
+     -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Form</title>
+    <script src="modernizr.custom.65897.js"></script>
+</head>
+    <body>
+        <?php
+            $showForm = true;
+            $errorCount = 0;
+            $sender = "";
+            $email = "";
+            $subject = "";
+            $message = "";
+        ?>
+    </body>
+</html>
